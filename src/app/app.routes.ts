d3bfdb4399e7d7386a36bfab7083765da2fac5ce
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2025-11-17 21:15:09 +0100

    Start Design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { StartscreenComponent } from './startscreen/startscreen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: StartscreenComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
