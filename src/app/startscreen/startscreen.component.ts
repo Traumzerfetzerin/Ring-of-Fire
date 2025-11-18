06382d9cc6fc36acee6bbb6435c38e107f03314b
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2025-11-18 19:40:23 +0100

    start game

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-startscreen',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './startscreen.component.html',
   styleUrl: './startscreen.component.scss'
 })
-export class StartscreenComponent {
+export class StartscreenComponent implements OnInit {
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  newGame() {
+    // Start a new game
+    this.router.navigateByUrl('/game');
+  }
 }
