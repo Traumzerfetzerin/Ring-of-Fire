79e46bf222456c6ac263ea923c4d57700bb7b088
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2025-11-21 19:49:41 +0100

    Design of the player bar

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
 
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -39,7 +40,11 @@ export class GameComponent implements OnInit {
   }
 
 
-
+  /**
+   * Animates the taking of a card from the stack.
+   * If the animation is not already running, it pops the top card from the stack and sets the currentCard property.
+   * It then sets the pickCardAnimation property to true and waits for 1000ms before adding the currentCard to the playedCards and resetting the pickCardAnimation property to false.
+   */
   takeCard() {
     if (!this.pickCardAnimation) {
       this.currentCard = this.game.stack.pop() as string;
