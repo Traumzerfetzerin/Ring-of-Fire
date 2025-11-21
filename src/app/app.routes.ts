2e5be7f809ba21d05efa9dd684ba59b1962fad8c
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2025-11-21 20:53:48 +0100

    Installing Material Design - Part 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StartscreenComponent } from './startscreen/startscreen.component';
 import { GameComponent } from './game/game.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 export const routes: Routes = [
     { path: '', component: StartscreenComponent },
     { path: 'game', component: GameComponent },
 ];
 
+
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [
+        RouterModule.forRoot(routes),
+        MatButtonModule,
+        MatIconModule
+    ],
     exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
