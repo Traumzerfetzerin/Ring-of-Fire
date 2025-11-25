c359dc86ae937dc31db494b19035a513cd4e0476
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2025-11-25 19:34:42 +0100

    Add player

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = ['Gunther', 'Nadja', 'Sigrun'];
+    public players: string[] = [];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
