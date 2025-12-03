dc49925dcc91370872a3fba01d38ca205a32982f
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2025-12-03 20:23:27 +0100

    Load game rules from JSON

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -1,16 +1,63 @@
-import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 
 @Component({
   selector: 'app-game-info',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
   ],
   templateUrl: './game-info.component.html',
-  styleUrl: './game-info.component.scss'
+  styleUrls: ['./game-info.component.scss']
 })
-export class GameInfoComponent {
 
+
+export class GameInfoComponent implements OnInit, OnChanges {
+
+
+  cardAction = [
+    { title: 'Waterfall', description: 'Everyone has to start drinking at the same time. As soon as player 1 stops drinking, player 2 may stop drinking. Player 3 may stop as soon as player 2 stops drinking, and so on.' },
+    { title: 'You', description: 'You decide who drinks' },
+    { title: 'Me', description: 'Congrats! Drink a shot!' },
+    { title: 'Category', description: 'Come up with a category (e.g. Colors). Each player must enumerate one item from the category.' },
+    { title: 'Bust a jive', description: 'Player 1 makes a dance move. Player 2 repeats the dance move and adds a second one.' },
+    { title: 'Chicks', description: 'All girls drink.' },
+    { title: 'Heaven', description: 'Put your hands up! The last player drinks!' },
+    { title: 'Mate', description: 'Pick a mate. Your mate must always drink when you drink and the other way around.' },
+    { title: 'Thumbmaster', description: '' },
+    { title: 'Men', description: 'All men drink.' },
+    { title: 'Quizmaster', description: '' },
+    { title: 'Never have i ever...', description: 'Say something you never did. Everyone who did it has to drink.' },
+    { title: 'Rule', description: 'Make a rule. Everyone needs to drink when he breaks the rule.' },
+  ];
+
+
+  title: string = '';
+  description: string = '';
+  @Input() card: string = '';
+
+
+  constructor() { }
+
+
+  /**
+   * Lifecycle hook that is called after Angular has finished initializing all the components.
+   * Called once by Angular after the first change detection check.
+   */
+  ngOnInit(): void {
+  }
+
+
+  /**
+   * Lifecycle hook that is called when Angular detects changes to input properties.
+   * Updates the title and description properties based on the given card.
+   */
+  ngOnChanges(): void {
+    if (this.card) {
+      let cardNumber = +this.card.split('_')[1];
+      this.title = this.cardAction[cardNumber - 1].title;
+      this.description = this.cardAction[cardNumber - 1].description;
+    }
+  }
 }
