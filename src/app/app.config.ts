d1ba61f4a3d3ea279d0d7fad3cc68dba6a5fccb2
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-01-06 21:22:12 +0100

    add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-402f2","appId":"1:21312350780:web:0edaefe86a73e61bc27139","storageBucket":"ring-of-fire-402f2.firebasestorage.app","apiKey":"AIzaSyB1VEM4uhxcFqi19lO3f9O812VHXwZ9l5Y","authDomain":"ring-of-fire-402f2.firebaseapp.com","messagingSenderId":"21312350780","projectNumber":"21312350780","version":"2"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
 };
