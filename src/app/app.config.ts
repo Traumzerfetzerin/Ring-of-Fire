a7912634dba0c32c55306249df5f599f4151ef5c
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-01-07 19:43:58 +0100

    Install AngularFire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,5 +8,5 @@ import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-402f2","appId":"1:21312350780:web:0edaefe86a73e61bc27139","storageBucket":"ring-of-fire-402f2.firebasestorage.app","apiKey":"AIzaSyB1VEM4uhxcFqi19lO3f9O812VHXwZ9l5Y","authDomain":"ring-of-fire-402f2.firebaseapp.com","messagingSenderId":"21312350780","projectNumber":"21312350780","version":"2"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-402f2","appId":"1:21312350780:web:0edaefe86a73e61bc27139","storageBucket":"ring-of-fire-402f2.firebasestorage.app","apiKey":"AIzaSyB1VEM4uhxcFqi19lO3f9O812VHXwZ9l5Y","authDomain":"ring-of-fire-402f2.firebaseapp.com","messagingSenderId":"21312350780"})), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyB1VEM4uhxcFqi19lO3f9O812VHXwZ9l5Y',
+        authDomain: 'ring-of-fire-402f2.firebaseapp.com',
+        databaseURL: 'https://ring-of-fire-402f2-default-rtdb.europe-west1.firebasedatabase.app',
+        projectId: 'ring-of-fire-402f2',
+        storageBucket: 'ring-of-fire-402f2.firebasestorage.app',
+        messagingSenderId: '21312350780',
+        appId: '1:21312350780:web:0edaefe86a73e61bc27139',
+    },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
